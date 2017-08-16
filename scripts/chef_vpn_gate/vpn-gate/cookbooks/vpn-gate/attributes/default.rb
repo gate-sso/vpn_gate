2bdccb2b5cda25d43bc61c6d1bca91b489b9cdd1
Jason Jeremy Iman <wearemarching@Jasons-MacBook-Pro.local>
2017-08-16 11:09:05 +0700

    [jason] fixing conflicts, moving attributes to environment variable

--- scripts/chef_vpn_gate/vpn-gate/cookbooks/vpn-gate/attributes/default.rb
+++ scripts/chef_vpn_gate/vpn-gate/cookbooks/vpn-gate/attributes/default.rb
@@ -1,19 +1,13 @@
-default['vpn']['leftid'] 	= "gate-vpn"
-default['vpn']['leftsubnet']	= "192.168.0.0/16"
-default['vpn']['rightsourceip']	= "192.168.0.2/16"
-default['vpn']['aggressive']	= true
-default['vpn']['psk']		= "defaultpassword"
+default['vpn']['leftid'] = "gate-vpn"
+default['vpn']['leftsubnet'] = "192.168.0.0/16"
+default['vpn']['rightsourceip'] = "192.168.0.2/16"
+default['vpn']['aggressive'] = true
+default['vpn']['psk'] = "defaultpassword"
 default['vpn']['pam_service']	= "gate-sso"
 
-default['nss_cache']['api_key']		= "defaultapikey"
-default['nss_cache']['gate_host']	= "https://192.168.1.1/nss"
+default['nss_cache']['api_key'] = ENV['NSS_API_KEY']
+default['nss_cache']['gate_host'] = ENV['NSS_GATE_HOST']
 
-<<<<<<< HEAD
-default['pam_gate']['auth_url'] = "https://127.0.0.1"
-default['pam_gate']['account_url'] = "https://127.0.0.1"
-default['pam_gate']['token'] = "defaulttoken"
-=======
 default['pam_gate']['url'] = ENV['GATE_URL']
 default['pam_gate']['token'] = ENV['GATE_TOKEN']
->>>>>>> fec21185ac552f18497b1102d599e7aba69eff68
 default['pam_gate']['min_user_id'] = "500"
