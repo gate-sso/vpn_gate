3d97a43f47814493264a9c8a2b256c17a4c8a825
root <jasongustaman@gmail.com>
2017-08-22 14:08:09 +0700

    [jason] fix shell script

--- scripts/chef_vpn_gate/setup_ipsec.sh
+++ scripts/chef_vpn_gate/setup_ipsec.sh
@@ -4,9 +4,9 @@
 [ -z "$NSS_GATE_HOST" ] && echo "Environment variable NSS_GATE_HOST is not set." && exit 1;
 [ -z "$GATE_URL" ] && echo "Environment variable GATE_URL is not set." && exit 1;
 [ -z "$GATE_TOKEN" ] && echo "Environment variable GATE_TOKEN is not set." && exit 1;
-which chef-solo
+which chef-solo > /dev/null
 if [ $? == 1 ]; then
   echo "chef-solo is not installed. You might want to run 'apt install chef.'" && exit 1;
 fi
 echo 'Running chef-solo...'
-chef-solo -c solo.rb. -j solo.json
+chef-solo -c solo.rb -j solo.json
