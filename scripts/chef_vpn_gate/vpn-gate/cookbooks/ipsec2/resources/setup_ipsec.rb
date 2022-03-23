b22a4ac1735f0d6381d17ac5c324d4adb8340c17
Akshay Agarwal <akshayagarwal2894@gmail.com>
2022-03-23 18:44:21 +0530

    fix: [GATE] remove certs which are not needed to avoid certificates conflict

--- scripts/chef_vpn_gate/vpn-gate/cookbooks/ipsec2/resources/setup_ipsec.rb
+++ scripts/chef_vpn_gate/vpn-gate/cookbooks/ipsec2/resources/setup_ipsec.rb
@@ -17,38 +17,9 @@ action :setup do
   end
 
   if app_name == "ipsec"
-    remote_file '/etc/ssl/certs/isrgrootx1.pem' do
-        source 'https://letsencrypt.org/certs/isrgrootx1.pem'
-        action :create
-    end
-
-    remote_file '/etc/ssl/certs/isrg-root-x1-cross-signed.pem' do
-        source 'https://letsencrypt.org/certs/isrg-root-x1-cross-signed.pem'
-        action :create
-    end
-
-    remote_file '/etc/ssl/certs/isrg-root-x2.pem' do
-        source 'https://letsencrypt.org/certs/isrg-root-x2.pem'
-        action :create
-    end
-
-    remote_file '/etc/ssl/certs/isrg-root-x2-cross-signed.pem' do
-        source 'https://letsencrypt.org/certs/isrg-root-x2-cross-signed.pem'
-        action :create
-    end
 
     remote_file '/etc/ssl/certs/lets-encrypt-r3.pem' do
         source 'https://letsencrypt.org/certs/lets-encrypt-r3.pem'
-        action :create
-    end
-
-    remote_file '/etc/ssl/certs/lets-encrypt-r3-cross-signed.pem' do
-        source 'https://letsencrypt.org/certs/lets-encrypt-r3-cross-signed.pem'
-        action :create
-    end
-
-    remote_file '/etc/ssl/certs/lets-encrypt-e1.pem' do
-        source 'https://letsencrypt.org/certs/lets-encrypt-e1.pem'
         action :create
     end
 
